d992fbd36c4082e26caac194ec011ec44f8587ec
mnimje <mayanknimje87@gmail.com>
2018-07-09 23:00:39 +0530

    Initial Code added to transaction
    Code added for Transactions

--- src/app/transaction-row/transaction-row.component.ts
+++ src/app/transaction-row/transaction-row.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, HttpModule } from '@angular/http' ;
+import { map } from "rxjs/Operators";
+import { Observable } from '../../../node_modules/rxjs/Observable';
 @Component({
   selector: 'app-transaction-row',
   templateUrl: './transaction-row.component.html',
   styleUrls: ['./transaction-row.component.css']
 })
 export class TransactionRowComponent implements OnInit {
-
-  constructor() { }
+  public Items = [];
+  constructor(private http : Http) {
+    
+    this.http.get("../assets/transactions.json").subscribe((data) => {
+      let response = data.json().data;
+      this.Items = response;
+      console.log(response);
+    });
+  }
 
   ngOnInit() {
   }
